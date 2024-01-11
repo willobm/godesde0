62da190a75ea7155a52b7e2daafcf3bb3b1cea70
Wilson F. <wgfloresh@gmail.com>
2024-01-10 23:51:32 -0500

    añadido paquete de resto de variables

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	variables.MuestroEnteros()
+	variables.RestoVariables()
 }
---
+++ variables/resto.go
@@ -0,0 +1,22 @@
+package variables
+
+import (
+	"fmt"
+	"time"
+)
+
+var Nombre string
+var Estado bool
+var Sueldo float32
+var Fecha time.Time
+
+func RestoVariables() {
+	Nombre = "Wilson"
+	Estado = true
+	Sueldo = 2323
+	Fecha = time.Now()
+	fmt.Println(Nombre)
+	fmt.Println(Estado)
+	fmt.Println(Sueldo)
+	fmt.Println(Fecha)
+}
