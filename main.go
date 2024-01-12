c5cb181d97d79ad83a034042128bccb9c71f7992
Wilson F. <wgfloresh@gmail.com>
2024-01-11 23:34:36 -0500

    Agregue las condiciones de if and switch

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
+
 	"github.com/willobm/godesde0/variables"
 )
 
 func main() {
-	variables.RestoVariables()
+	estado, texto := variables.ConveritoaTexto(100)
+	if estado {
+		fmt.Println(texto)
+	} else {
+		fmt.Println("No pudo convertir a texto")
+	}
+
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("No es un Windows, es: ", os)
+	} else {
+		fmt.Println("Es un Windows: ", os)
+	}
+
+	switch os := runtime.GOOS; os {
+	case "windows":
+		fmt.Println("Esto es un windows")
+	case "darwin":
+		fmt.Println("Esto es un darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
--- variables/resto.go
+++ variables/resto.go
@@ -2,6 +2,7 @@ package variables
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -19,4 +20,10 @@ func RestoVariables() {
 	fmt.Println(Estado)
 	fmt.Println(Sueldo)
 	fmt.Println(Fecha)
+}
+
+func ConveritoaTexto(numero int) (bool, string) {
+
+	texto := strconv.Itoa(numero)
+	return true, texto
 }
